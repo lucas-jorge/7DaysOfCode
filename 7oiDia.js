50f01ad9d0d593381f81f7b6a2acbdd7edb06c8b
lucas-jorge <lucasajorge@gmail.com>
2023-08-04 14:52:06 -0300

    calculadora com função de menu

--- 7oiDia.js
+++ 7oiDia.js
@@ -1,36 +1,55 @@
 const prompt = require("prompt-sync")({ sigint: true });
 
+menu(); // call the menu function
+let operacao = prompt("Digite a operação desejada: ");
 let numero = parseFloat(prompt("Digite um número: "));
-let operacao = prompt("Escolha qual operação deseja fazer (+, -, *, /): ");
 let numero2 = parseFloat(prompt("Digite outro número: "));
 let resultado;
 
-if (operacao === "+") {
+switch (operacao) { // use a switch statement
+  case "1":
     resultado = soma(numero, numero2);
-} else if (operacao === "-") {
+    break;
+  case "2":
     resultado = subtracao(numero, numero2);
-} else if (operacao === "*") {
+    break;
+  case "3":
     resultado = multiplicacao(numero, numero2);
-} else if (operacao === "/") {
+    break;
+  case "4":
     resultado = divisao(numero, numero2);
-} else {
-    console.log("Operação inválida.");
+    break;
+  default:
+    console.log("Operação inválida."); // display an error message
 }
 
-console.log("Resultado: " + resultado);
+console.log(`Resultado: ${resultado}`); // use template literals
+
+function menu() {
+  console.log("Bem vindo a calculadora, temos as seguintes operações:");
+  console.log("1 - Soma");
+  console.log("2 - Subtração");
+  console.log("3 - Multiplicação");
+  console.log("4 - Divisão");
+  console.log("0 - Sair");
+}
 
 function soma(numero, numero2) {
-    return numero + numero2;
+  return numero + numero2;
 }
 
 function subtracao(numero, numero2) {
-    return numero - numero2;
+  return numero - numero2;
 }
 
 function multiplicacao(numero, numero2) {
-    return numero * numero2;
+  return numero * numero2;
 }
 
 function divisao(numero, numero2) {
-    return numero / numero2;
+  if (numero2 === 0) { // check for division by zero
+    console.log("Não é possível dividir por zero."); // display an error message
+    return;
+  }
+  return numero / numero2;
 }
